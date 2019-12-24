c462d86d86f48d23b73c8af9eda1578bb7b151b3
Rasha Hantash <rasha.hantash01@gmail.com>
2019-12-23 19:33:29 -0500

    mergetwolists.go

--- main.go
+++ main.go
@@ -3,9 +3,34 @@ package main
 import (
 	"fmt"
 )
-func main() {
-	// b := []int{8,20,19,2,14,9}
-	// fmt.Println(twoSum(b, 17))
 
-	fmt.Println(isValid("{[]}"))
+//ListNode for singly-linked list.
+type ListNode struct {
+    Val int
+	Next *ListNode
 }
+
+
+func main() {
+	//a := []int{8,20,19,2,14,9}
+	//b := []int{8,20,19,2,14,9}
+	// fmt.Println(twoSum(b, 17))
+	l1 := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{Val: 4}}}}
+	l2 := &ListNode{2, &ListNode{2, &ListNode{4, &ListNode{Val: 6}}}}
+
+	r3 := mergeTwoLists(l1, l2)
+	display(r3)
+}
+
+//displays the linked list
+func display(list *ListNode) {
+    for list != nil {
+        fmt.Printf("%v ->", list.Val)
+        list = list.Next
+    }
+    fmt.Println()
+}
+
+
+
+
